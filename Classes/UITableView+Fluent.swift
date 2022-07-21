447e766eb275f18e1e961ddb298422abd48ef667
Ven <>
2022-07-21 18:05:56 +0800

    Add TableView method

--- Classes/UITableView+Fluent.swift
+++ Classes/UITableView+Fluent.swift
@@ -20,4 +20,34 @@ extension UITableView {
         register(cellClass, forHeaderFooterViewReuseIdentifier: identifier)
         return self
     }
+    
+    @discardableResult public func setSeparatorStyle(_ style: UITableViewCell.SeparatorStyle) -> Self {
+        separatorStyle = style
+        return self
+    }
+    
+    @discardableResult public func setSeparatorColor(_ color: UIColor) -> Self {
+        separatorColor = color
+        return self
+    }
+    
+    @discardableResult public func setAllowsSelection(_ allow: Bool) -> Self {
+        allowsSelection = allow
+        return self
+    }
+    
+    @discardableResult public func setAllowsSelectionDuringEditing(_ allow: Bool) -> Self {
+        allowsSelectionDuringEditing = allow
+        return self
+    }
+    
+    @discardableResult public func setAllowsMultipleSelection(_ allow: Bool) -> Self {
+        allowsMultipleSelection = allow
+        return self
+    }
+    
+    @discardableResult public func setAllowsMultipleSelectionDuringEditing(_ allow: Bool) -> Self {
+        allowsMultipleSelectionDuringEditing = allow
+        return self
+    }
 }
